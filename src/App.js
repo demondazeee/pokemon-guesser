128913bcaae4f2bfd19b4f40bcb6f41e3867cf54
Bryan Avila <bryanjimavila@gmail.com>
2022-11-07 19:54:01 +0800

    added new mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useContext } from "react";
+import HeaviestPokemon from "./component/Guess/HeaviestPokemon";
+import TallestPokemon from "./component/Guess/TallestPokemon";
+import { dataCtx } from "./store/DataContext";
 
-import TallestPokemon from "./component/TallestPokemon";
 
 
 
 
 function App() {
+  // const [guess, setGuess] = useState('')
 
+  // const guessHandler = (mode) => {
+  //   setGuess(mode)
+  // }
+  
+  const {mode, modeHandler} = useContext(dataCtx)
   return (
     <>
       <main className="mx-auto py-6 max-w-2xl">
-        <TallestPokemon />
+        <nav>
+          <ul className="flex gap-2">
+            <li><button href="#" onClick={modeHandler.bind(this,'tallest')}>Tallest Pokemon</button></li>
+            <li><button href="#" onClick={modeHandler.bind(this, 'heaviest')}>Heaviest Pokemon</button></li>
+          </ul>
+        </nav>
+        {mode === 'tallest' && <TallestPokemon />}
+        {mode === 'heaviest' && <HeaviestPokemon />}
       </main>
     </>
   );
